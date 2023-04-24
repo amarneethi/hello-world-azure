fe174aeecd0edf25553cc6711d43be0093907f82
amarneethi <amarneethi@gmail.com>
2023-04-24 21:10:20 +0530

    Submit data and receive response

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'Amarneethi' })
+  const {name} = req.body
+  res.status(200).json({name:`${name} from the API`})
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,42 @@
-import { useState, useEffect } from "react"
+import { useState } from "react"
 
 
 export default function Home() {
 
-  const [data, setData] = useState('')
+  const [name, setName] = useState('')
 
-  useEffect(() => {
-    fetch('api/hello')
-      .then(res => res.json())
-      .then(data => setData(data.name))
-  }, [])
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    const res = await fetch('api/hello', {
+      method: 'POST',
+      body: JSON.stringify({ name }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+
+    const data = await res.json()
+    setName (data.name)
+    
+
+  }
+
+
 
 
 
   return (
     <>
       <h1>Hello World</h1>
-      <p>{`This is the name from the api - ${data}` }</p>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:
+          <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+
+        </label>
+        <button type="submit">Submit</button>
+      </form>
+      {name && <p>{`This is the name from the form - ${name}`}</p>}
     </>
   )
 }
